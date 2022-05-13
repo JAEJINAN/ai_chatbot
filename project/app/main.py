a8a81c9444bb3a523324a336f9b4acbe38ebcb2f
JAEJINAN <ahn43541@gmail.com>
2022-05-14 06:38:13 +0900

    create static file

--- project/app/main.py
+++ project/app/main.py
@@ -5,21 +5,61 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 import uvicorn
 
+from datetime import datetime
+
 BASE_DIR = Path(__file__).resolve().parent  # project directory
-print(BASE_DIR)
-print(BASE_DIR)
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory=BASE_DIR)
 
+current_time = f'{datetime.now().hour: >2} : {datetime.now().minute: >2}'
+print(current_time)
 
+# 메인(로딩) 화면
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("templates/main.html", {"request": request,
-                                                              "title": "영한챗봇"})
+    return templates.TemplateResponse("templates/index.html",
+                                      {"request": request,
+                                       "title": "영한챗봇",
+                                       "time": current_time})
+
+
+# 채팅창
+@app.get('/chattings', response_class=HTMLResponse)
+async def chattings(request: Request):
+    return templates.TemplateResponse("templates/chattings.html", {"request": request,
+                                                                   "time": current_time, })
+
+
+# 비디오 화면
+@app.get('/videos', response_class=HTMLResponse)
+async def videos(request: Request):
+    return templates.TemplateResponse("templates/videos.html", {"request": request,
+                                                                "video_url": "https://www.youtube.com/embed/GjsB6GcbcN8",
+                                                                "time": current_time, })
+
+
+# 유저화면
+@app.get('/users', response_class=HTMLResponse)
+async def users(request: Request):
+    return templates.TemplateResponse("templates/users.html", {"request": request,
+                                                               "time": current_time, })
+
+
+# 채팅 내용 확인
+@app.get('/chatlogs', response_class=HTMLResponse)
+async def chatlogs(request: Request):
+    return templates.TemplateResponse("templates/chatlogs.html", {"request": request,
+                                                                  "time": current_time, })
+
+
+# 감정 요약 화면
+# @app.get('/summaries', response_class=HTMLResponse)
+# async def chatlogs(request: Request):
+#     return templates.TemplateResponse("templates/summaries.html", {"request": request})
 
 
 if __name__ == "__main__":
