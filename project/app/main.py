eee49b4b776e68a653e1f4ea8736a30cd194518e
JAEJINAN <ahn43541@gmail.com>
2022-05-13 21:45:55 +0900

    update image

--- project/app/main.py
+++ project/app/main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+import uvicorn
 
 BASE_DIR = Path(__file__).resolve().parent  # project directory
-
+print(BASE_DIR)
 print(BASE_DIR)
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+templates = Jinja2Templates(directory=BASE_DIR)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("main.html", {"request": request})
+    return templates.TemplateResponse("templates/main.html", {"request": request,
+                                                              "title": "영한챗봇"})
 
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
